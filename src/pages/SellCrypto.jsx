e4d9a063a68769df070d08fa5c0c9b42dc88869c
nauman-lab <naumanrasheed269@gmail.com>
2024-01-29 18:52:41 +0500

    theme config

--- src/pages/SellCrypto.jsx
+++ src/pages/SellCrypto.jsx
@@ -1,7 +1,17 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 const SellCrypto = () => {
-  return <div>SellCrypto</div>;
+  return (
+    <div>
+      <Typography variant="h1">Buy & Sell Crypto</Typography>
+      <Typography variant="h2">Buy & Sell Crypto</Typography>
+      <Typography variant="h3">Buy & Sell Crypto</Typography>
+      <Typography variant="h4">Buy & Sell Crypto</Typography>
+      <Typography variant="h5">Buy & Sell Crypto</Typography>
+      <Typography variant="h6">Buy & Sell Crypto</Typography>
+    </div>
+  );
 };
 
 export default SellCrypto;
